22022ba4eeff1d84b21ef3996ac5e09a9c5c0d33
suraj gupta <sandygupta0414@gmail.com>
2025-10-24 19:20:52 +0530

    day6 added

--- day6.js
+++ day6.js
@@ -1,1 +1,107 @@
-console.log('added');
+console.log('day6 started');
+
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+// Sum of even and odd numbers
+
+
+/**
+ * Function to sum all odd integers from 1 to n.
+ * @param {number} n - The number up to which odd integers will be summed.
+ * @returns {number} - Returns the sum of all odd integers from 1 to n.
+ */
+// function odd_sum(n){
+//     /*Function to sum odd integers*/
+//     let oddsum = 0;
+
+//     for (let i = 1; i <= n; i++){
+//         if (i % 2 !== 0) {
+//             oddsum = oddsum + i
+//         }
+//     }
+    
+//     return oddsum
+// }
+
+// /**
+//  * Function to sum all even integers from 1 to n.
+//  * @param {number} n - The number up to which even integers will be summed.
+//  * @returns {number} - Returns the sum of all even integers from 1 to n.
+//  */
+// function even_sum(n){
+//     /*Function to sum even integers*/
+//   let evensum = 0;
+
+//     for (let i = 1; i <= n; i++){
+//         if (i % 2 == 0) {
+//             evensum = evensum + i
+//         }
+//     }
+    
+//     return evensum
+    
+    
+    
+// }
+
+
+//2
+
+// Calculate the Area of a Rectangle Using *
+
+// function areaOfRectangle(length, width) {
+//     //write the code here
+    
+//     return length*width
+    
+//     //dont change the below code. If changed click on reset.
+   
+// }
+
+// console.log(areaOfRectangle(10,4))
+
+
+//3
+// While loop - power of a number
+
+// function power(base, exponent) {
+//   return base ** exponent
+// }
+// console.log(power(4,6))
+
+//4
+// Print Even Numbers in Reverse
+// function printEvenNumbersInReverse(n) {
+//     // For loop to print even numbers in reverse
+//     let even = []
+//     for (let i = 1; i <= n; i++){
+//         if (i % 2 == 0) {
+//             even.push(i)
+//         }
+//     }
+//     let x = even.reverse()
+//     for (let temp of x) {
+//         console.log(temp)
+//      }
+  
+// }
+
+
+//5
+// Problem Description:
+
+// Write a program that divides two numbers and prints the integer result.
+
+// The program will take two integer inputs, numerator and denominator, and then output the result of the division.
+
+// The division should be performed using the integer division operator (/), which discards the decimal part and returns the integer quotient of the division.
+
+
+// function divideTwoNumbers(numerator, denominator) {
+//     // Write your code here
+//      return numerator/denominator
+// }
+
+console.log('day6 ended');
+
