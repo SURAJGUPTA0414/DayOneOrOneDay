10026fbf41ad974242c2768aeddc9b99cad3cda5
suraj gupta <sandygupta0414@gmail.com>
2025-10-27 18:21:29 +0530

    ahead of practice

---
+++ day10.js
@@ -0,0 +1,12 @@
+console.log('day9 started');
+
+// function lengthOfLastWord(s) {
+//     let ff = s.trim()
+//     // Write your code here
+//     let x = ff.split(' ')
+//     // console.log(x)
+//     return (x[x.length -1]).length
+// }
+
+
+console.log('day9 ended');
