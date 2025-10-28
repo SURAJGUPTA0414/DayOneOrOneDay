c837cbef24602b34d80566e04739b468245d2607
suraj gupta <sandygupta0414@gmail.com>
2025-10-28 19:45:43 +0530

    added day10

--- day10.js
+++ day10.js
@@ -1,5 +1,5 @@
 console.log('day9 started');
-
+//1
 // function lengthOfLastWord(s) {
 //     let ff = s.trim()
 //     // Write your code here
@@ -8,5 +8,71 @@ console.log('day9 started');
 //     return (x[x.length -1]).length
 // }
 
+// 2
+//Swap Two Numbers Without Using an Extra Variable
+
+
+// function swapNumbers(a, b) {
+
+//   a = a + b;
+//   b = a - b;
+//   a = a - b;
+    
+//   return [a, b];
+// }
+
+//3
+
+// function checkNum(age){
+//     //write the code here
+
+//     switch(true) {
+//       case age >= 18: return true
+    
+//     break;
+  
+//   default:
+//             return false
+// }
+    
+//     //dont change the below code. If changed click on reset.
+   
+// }
+
+//4
+
+// Determine the vowel using switch case
+// function checkVowel(ch){
+//     //write the code here
+
+//     switch (ch) {
+//         case 'A': return true;
+//             break;
+//         case 'E': return true;
+//             break;
+//             case 'I': return true;
+//             break;
+//             case 'O': return true;
+//             break;
+//             case 'U': return true;
+//             break;
+//         default:
+//             return false;
+
+//     }
+    
+    
+    
+//     //dont change the below code. If changed click on reset.
+   
+// }
+
+//5
+// Maximum element in an array.
+
+// function find_maximum(arr) {
+//   return Math.max(...arr)
+
+// }
 
 console.log('day9 ended');
