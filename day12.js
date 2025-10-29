3217ab6d82bc18552f1aa9ad31135979f61d6a1b
suraj gupta <sandygupta0414@gmail.com>
2025-10-29 21:03:59 +0530

    ahead

--- day12.js
+++ day12.js
@@ -1,5 +1,32 @@
 console.log('day 12 started');
 
+//1
+// First and Last Occurence of Element
+// var element_search = function (nums, target) {
+//     let x1 = nums.indexOf(target)
+//     let x2 = nums.lastIndexOf(target)
 
+//      return [x1,x2]
+    
+// };
+
+//2
+// Check for Perfect Number (Sum of Divisor =
+
+// function isPerfectNumber(n) {
+//     let sum = 0;
+
+//     for (let i = 1; i < n; i++){
+//         if (n % i === 0) {
+//             sum = sum + i
+//         }
+//     }
+//     if (n === sum) {
+//         return true
+//     } else {
+//         return false
+//     }
+
+// }
 
 console.log('day 12 ended');
