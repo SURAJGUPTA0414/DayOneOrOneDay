a1ee1ed18294b856b6111db097e25061e6768861
suraj gupta <sandygupta0414@gmail.com>
2025-10-31 21:45:39 +0530

    day13 added

--- day13.js
+++ day13.js
@@ -1,16 +1,56 @@
 console.log('day 12 started');
 //1Find the GCD of Two Numbers
 
-function findGCD(num1, num2) {
-    let x = []
-    for (let i = 1; i <= num2 ; i++){
-        if (num1 % i === 0 && num2 %i ===0) {
-            // console.log(i);
-            x.push(i)
-        }
-    }
-      return x[x.length - 1]
-}
+// function findGCD(num1, num2) {
+//     let x = []
+//     for (let i = 1; i <= num2 ; i++){
+//         if (num1 % i === 0 && num2 %i ===0) {
+//             // console.log(i);
+//             x.push(i)
+//         }
+//     }
+//       return x[x.length - 1]
+// }
+
+//2
+
+// Reverse words in a string
+// var reverseWords = function(s) {
+//     return s.trim().split(/\s+/).reverse().join(' ')
+// };
+
+//3
+// Sum of the first n natural numbers.
+// var sumOfNaturalNumbers = function (n) {
+//     let sum = 0;
+//     for (let i = 1; i <= n; i++){
+//         sum = sum + i
+//     }
+//     return sum
+// };
+
+//4
+// Sum of digits
+
+// var sumOfDigits = function (num) {
+
+//     let x = num.toString().split('')
+//     let sum = 0
+//     for (let xx of x) {
+//         sum = sum + (+xx)
+//     }
+//     return (sum)
+    
+// };
+
+//5
+// Union of two arrays
+// var union = function (nums1, nums2) {
+//       let x = [...nums1,...nums2]
+//     let ff = [...new Set(x)]
+//     return ff
+// };
+
 
 
 console.log('day 13 ended');
