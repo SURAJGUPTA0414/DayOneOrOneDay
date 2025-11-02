19474462e72d10763c6c34fdaf5cf8ccebca90e0
suraj gupta <sandygupta0414@gmail.com>
2025-11-03 00:56:49 +0530

    day16 done

--- day16.js
+++ day16.js
@@ -28,5 +28,48 @@ console.log('day16 started');
   
 // }
 
+//3
+// Check whether a number is power of 2
+// function isPowerofTwo(n)
+// {
+//   let x;
+//   for (let i = 1; i*i <= n ; i++){
+   
+//     // console.log(i * i)
+//   x = i*i
+//   }
+//   if (x === n) {
+//     return 'YES'
+//   } else {
+//     return 'NO'
+//   }
+
+//     }
+    
+    //4
+    // Remove duplicate characters
+//     var removeDuplicates = function (s) {
+//     let x = s.split('');
+//     // let y = x.filter((a) => x.indexOf(a) !== x.lastIndexOf(a));
+//     return([...new Set(x)].join(''))
+    
+// };
+
+//5
+
+// class Solution {
+//     /**
+//      * Function to check if two numbers have opposite signs.
+//      * @param {number} a - The first integer.
+//      * @param {number} b - The second integer.
+//      * @return {boolean} True if they have opposite signs, otherwise False.
+//      */
+//     haveOppositeSigns(a, b) {
+//         // Implementation here
+//       return a * b < 0;
+        
+//     }
+// }
+
 
 console.log('day 16 ended');
