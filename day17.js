ac7164a7acdc322369780a17337c09e2c72978db
suraj gupta <sandygupta0414@gmail.com>
2025-11-03 00:58:58 +0530

    ahead

--- day17.js
+++ day17.js
@@ -1,4 +1,44 @@
 console.log("Day 17 started");
 
+//1
+// Maximum Occurring Character
+// var maxOccuringChar = function(s) {
+//     let x = s.split('');
+
+//     let y = x.filter((a) => x.indexOf(a) !== x.lastIndexOf(a));
+//   return(y[0])
+// };
+
+//half logic above
+
+//2
+
+// Question:
+// Write a program that takes an integer N as input and prints the following pattern for N rows:
+
+
+
+// A  
+
+// AB  
+
+// ABC  
+
+// ABCD  
+
+// ABCDE  
+function printAlphabetPattern(n) {
+    for (let i = 1; i <= n; i++){
+        let str = "";
+        for (let j = 1; j <= i ; j++){
+            str += String.fromCharCode(64 + j)
+        }
+        console.log(str)
+    }
+
+}
+
+
+
 
 console.log('day17 ended');
