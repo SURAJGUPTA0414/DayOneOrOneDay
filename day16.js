cbdb78af802a0973e92c43b25399574bb975d40e
suraj gupta <sandygupta0414@gmail.com>
2025-11-02 13:46:44 +0530

    ahead

--- day16.js
+++ day16.js
@@ -1,3 +1,32 @@
 console.log('day16 started');
 
+//1
+// Swap Two Variables Using XOR Operator Only
+// function swap(a, b) {
+//     a = a ^ b;
+//     b = a ^ b;
+//     a = a ^ b;
+
+//     // don't change below code
+//     console.log('a value is =', a);
+//     console.log('b value is =', b);
+// }
+
+//2
+// Check if an Array is Sorted
+
+// function isSorted(arr) {
+
+
+//     for (let i = 0; i < arr.length - 1; i++) {
+//     //left vala bada hua to false else true
+//         if (arr[i] > arr[i + 1]) {
+//             return false; 
+//         }
+//     }
+//     return true;
+  
+// }
+
+
 console.log('day 16 ended');
