7e165ad227e0eaeea222a15b4e08a92c22b6e87c
suraj gupta <sandygupta0414@gmail.com>
2025-11-04 20:18:50 +0530

    day17 done

--- day17.js
+++ day17.js
@@ -11,34 +11,75 @@ console.log("Day 17 started");
 
 //half logic above
 
-//2
+//2 Pattern of Consecutive Numbers
 
-// Question:
-// Write a program that takes an integer N as input and prints the following pattern for N rows:
+// function printConsecutivePattern(n) {
+//     let num = 1;
+//   for (let i = 1; i <= n; i++) {
+//     let line = '';
+//     for (let j = 1; j <= i; j++) {
+//       line += num + ' ';
+//       num++;
+//     }
+//     console.log(line.trim());
+//   }
+
+// }
+
+//3
+// Print Pattern using Alphabets -1
+
+// function printAlphabetPattern(n) {
+//     for (let i = 1; i <= n; i++){
+//         let str = "";
+//         for (let j = 1; j <= i ; j++){
+//             str += String.fromCharCode(64 + j)
+//         }
+//         console.log(str)
+//     }
+
+// }
+
+//4
+// Reverse Staircase pattern for alphabets
+
+// function printReverseAlphabetPattern(n) {
+//   for (let i = n; i >= 1; i--) {
+//     let str = "";
+//     for (let j = i; j <= n; j++) {
+//       str += String.fromCharCode(64 + j);
+//     }
+//     console.log(str);
+//   }
+// }
+//5
 
 
+// Pattern-4
 
-// A  
+// ****
+//  ***
+//   **
+//    *
+   
+//   based on n
+   
 
-// AB  
+// function pattern(n) {
+//   for (let i = 0; i < n; i++) {
+//     let str = "";
 
-// ABC  
+//     // First loop: print i spaces
+//     for (let j = 0; j < i; j++) {
+//       str += " ";
+//     }
 
-// ABCD  
+//     // Second loop: print (n - i) asterisks
+//     for (let j = 0; j < n - i; j++) {
+//       str += "*";
+//     }
 
-// ABCDE  
-function printAlphabetPattern(n) {
-    for (let i = 1; i <= n; i++){
-        let str = "";
-        for (let j = 1; j <= i ; j++){
-            str += String.fromCharCode(64 + j)
-        }
-        console.log(str)
-    }
-
-}
-
-
-
-
+//     console.log(str);
+//   }
+// }
 console.log('day17 ended');
