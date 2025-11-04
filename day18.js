d3b45cb74f4f56d0ca7baf308051d23fba149922
suraj gupta <sandygupta0414@gmail.com>
2025-11-04 20:20:27 +0530

    ahead

--- day18.js
+++ day18.js
@@ -1,4 +1,14 @@
 console.log("Day 18");
 
+//1
+// Find the Intersection of Two Sorted Arrays
+// class Solution {
+//     findIntersection(nums1, nums2) {
+//         let x = [...nums1, ...nums2];
+//         let y = x.filter((a) => x.indexOf(a) !== x.lastIndexOf(a));
+//         return [([...new Set(y)].join(' '))]; // Placeholder for function implementation
+//     }
+// }
+
 
 console.log("Day 18 ended");
