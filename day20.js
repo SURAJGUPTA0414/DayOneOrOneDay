6d40723a9c9dfc50c98588d9b831b6e45444e083
Gupta <suraj.gupta@capgemini.com>
2025-11-06 23:59:29 +0530

    ahead

---
+++ day20.js
@@ -0,0 +1,12 @@
+console.log("day 20");
+//1
+// Print All Factors
+// function printFactors(n) {
+//     for (let i = 1; i <= n; i++) {
+//         if (n % i === 0) {
+//             console.log(i);
+//         }
+//     }
+// }
+// printFactors(12);
+console.log("Day 20 ended done");
