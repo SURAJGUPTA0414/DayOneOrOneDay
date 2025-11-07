f55051aec39a11c4401ff3eb987bd047b37a8459
suraj gupta <sandygupta0414@gmail.com>
2025-11-07 23:44:20 +0530

    ahead

--- day21.js
+++ day21.js
@@ -1,3 +1,30 @@
 console.log("Day 21 started");
 
+//1
+// Detect if Two Numbers Have Opposite Signs
+class Solution {
+    /**
+     * Function to check if two numbers have opposite signs.
+     * @param {number} a - The first integer.
+     * @param {number} b - The second integer.
+     * @return {boolean} True if they have opposite signs, otherwise False.
+     */
+    haveOppositeSigns(a, b) {
+        // Implementation here
+       
+        let x = a * b;
+         if (x === 0) {
+            return false
+        }
+        if (x > 0) {
+            return false
+        } else {
+            return true
+        }
+        
+        
+    }
+}
+
+
 console.log("Day 21 ended done");
