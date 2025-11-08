d1c1324e91a741f0b068907d23bad71fa03ad32b
suraj gupta <sandygupta0414@gmail.com>
2025-11-08 20:38:26 +0530

    ahead

--- day22.js
+++ day22.js
@@ -1,4 +1,25 @@
 console.log('day22 started');
 
+//5
+// Check for prime number
+
+// function prime(n){
+//     let flag ;
+    
+//     for(let i=2;i<n;i++){
+//         if(n%i === 0){
+//           flag = true;
+//           break;
+//         }
+//     }
+//     if(flag){
+//         console.log('not prime')
+//     }else{
+//         console.log('prime')
+//     }
+    
+// }
+// prime(8)
+
 
 console.log('day 22 ended done');
