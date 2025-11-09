695a76677008c61a7f41ca7a5ef05fe21ef75339
Gupta <suraj.gupta@capgemini.com>
2025-11-09 21:41:46 +0530

    day22 done

--- day22.js
+++ day22.js
@@ -1,6 +1,6 @@
 console.log('day22 started');
 
-//5
+//1
 // Check for prime number
 
 // function prime(n){
@@ -21,5 +21,78 @@ console.log('day22 started');
 // }
 // prime(8)
 
+//2
+// Print Perfect Squares in a Range
 
-console.log('day 22 ended done');
+// function perfectSquare(n){
+//     for(let i=1;i*i<=n;i++){
+//         console.log(i*i)
+//     }
+    
+// }
+// perfectSquare(16)
+
+//3
+// Search an Element
+// In your quest, you may encounter the following scenario:
+// Input:
+// 8
+// 3 17 9 25 12 5 14 21
+// 12
+// Output:
+// 4
+
+// function searchForElement(arr, length, target) {
+//         // Your implementation here
+      
+// console.log(arr,length,target);
+
+// for(let i=0;i<=arr.length-1;i++){
+//     if(arr[i]===target){
+//         return i
+//     }
+    
+// }
+// return -1
+        
+//     }
+    
+    
+//     searchForElement([3 ,17 ,9, 25 ,12, 5 ,14 ,21],8,12)
+
+//4
+
+//
+// count zeros
+// function countZeros(nums) {
+//   let xx = nums.toString().split('');
+//   let zeros = [];
+//   for(let x of xx){
+//      let dd = +x
+//       if(dd === 0){
+//           zeros.push(dd)
+//       }
+//   }
+//   console.log(zeros.length)
+// };
+
+// console.log(countZeros(303023009800))
+
+//5
+// Missing number in array
+// function missingArray(arr){
+   
+//     let x = []
+//     for(let i=1;i<=Math.max(...arr);i++){
+//         x.push(i)
+//     }
+//     let combined = [...arr,...x];
+//     console.log(combined)
+//     let miss = combined.filter((a)=>combined.indexOf(a)===combined.lastIndexOf(a)) 
+//   console.log(miss)
+// }
+
+// missingArray([1,4,8])
+
+
+console.log('day 22 ended done ....');
