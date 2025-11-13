b09b71e05817aee6785f2865d18c108651b0be77
suraj gupta <sandygupta0414@gmail.com>
2025-11-13 19:38:57 +0530

    day26 done

--- day26.js
+++ day26.js
@@ -1,3 +1,45 @@
 console.log("Day 26 started");
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+//1
+// calculate age in days
+
+// function calcAge(age) {
+// 	return age*365
+// }
+
+// calcAge(27)
+
+//2
+//convert min into sec
+
+// function convert(minutes) {
+// 	return minutes*60
+	
+// }
+// convert(40)
+
+//3
+// Maximum Edge of a Triangle
+// function nextEdge(side1, side2) {
+// 	return (side1 + side2) - 1
+	
+// }
+
+//4
+// Power Calculator
+// function circuitPower(voltage, current) {
+// 	let x = voltage * current
+// 	return x
+	
+// }
+
+//5
+// Sum of Polygon Angles
+// function sumPolygon(n) {
+// 	return  (n - 2) * 180
+// }
+
 
 console.log("Day 26 ended done");
