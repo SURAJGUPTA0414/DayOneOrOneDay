3022000b8498fd5fb15108dd513af5975024f525
suraj gupta <sandygupta0414@gmail.com>
2025-11-15 15:53:51 +0530

    will add remainnig in sometime

--- day28.js
+++ day28.js
@@ -1,4 +1,60 @@
 console.log('day 28 started');
 //1
+// Write a function to reverse a string without using built-in reverse.
+
+// function reversestrwithoutinbuilt(str){
+//     let revereName = []
+//     for(let i=str.length-1;i>=0;i--){
+//         revereName.push(str[i])
+//     }
+//     console.log(revereName.join(''))
+// }
+
+// reversestrwithoutinbuilt('slsl')
+
+
+//2
+
+// Find the maximum occurring character in a string.
+
+// function maxoccurechsr(str){
+//     let x = str.split('');
+//     // let dd = x.filter((a)=>x.indexOf(a)!==x.lastIndexOf(a));
+//     // console.log(dd)
+//     let obj = {}
+//     for(let xx of x){
+//         if(obj[xx]){
+//             obj[xx] = obj[xx]+1
+//         }else{
+//             obj[xx] =1
+//         }
+//     }
+//     let maxvalue = Math.max(...Object.values(obj));
+//     let key = Object.keys(obj).find(key => obj[key] === maxvalue);
+//     console.log('max occure char is :',key)
+    
+// }
+// maxoccurechsr('skkkkkjfjsnkfjss')
+
+//3
+// Given an array, check if it is sorted in ascending order.
+
+// function isSorted(arr) {
+//   for (let i = 0; i < arr.length - 1; i++) {
+//     if (arr[i] > arr[i + 1]) {
+//       return false; // found a violation
+//     }
+//   }
+//   return true; // no violations
+// }
+
+// console.log(isSorted([1, 2, 3, 4])); // true
+// console.log(isSorted([1, 3, 2, 4])); // false
+
+
+//4
+
+//5
+
 
 console.log('day 28 ended done');
