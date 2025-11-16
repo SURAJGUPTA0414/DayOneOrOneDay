29a2a03164dec72f2e99dd2f3386d128eae610d7
suraj gupta <sandygupta0414@gmail.com>
2025-11-16 20:25:05 +0530

    ahead

--- day30.js
+++ day30.js
@@ -1,5 +1,29 @@
 console.log('day 30 started');
 //alpa p
 
+//1 reverse a word in a sentence
+
+
+//1 reverse a word in a sentence
+
+function reversewordinsentence(word){
+    let x = word.split(' ');
+    // console.log(x)
+    let reverse = [];
+    
+    for(let ff of x){
+        
+        let x = ff.split('').reverse().join('');
+        // console.log(x);
+        reverse.push(x)
+    }
+    console.log(reverse.join(' ').toUpperCase())
+    
+}
+
+reversewordinsentence('the sun is shinny')
+
+
+
 
 console.log('day 30 ended');
