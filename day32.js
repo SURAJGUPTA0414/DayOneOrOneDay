7cf2d609eceb15f7fdd119c9e1b6005f1c6967ff
suraj gupta <sandygupta0414@gmail.com>
2025-11-18 20:40:11 +0530

    ahead of prc

--- day32.js
+++ day32.js
@@ -1,4 +1,24 @@
 console.log('day 32 started');
 
+//1
+// capitalize first letter of each word in a string
+//1
+function firstwordcapitalize(str){
+  
+    let xx = str.split(' ')
+      console.log(xx);
+      
+      for(let dd of xx){
+          let gg = dd.split('')
+        //   console.log(gg)
+          gg[0] = gg[0].toUpperCase();
+          console.log(gg.join(''))
+      }
+}
+
+firstwordcapitalize('i am suraj')
+
+
+
 
 console.log('day 32 ended');
