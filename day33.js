3112973492b03fb705e380712db7c746fcbb7546
suraj gupta <sandygupta0414@gmail.com>
2025-11-19 20:40:25 +0530

    ahead

--- day33.js
+++ day33.js
@@ -1,4 +1,11 @@
 console.log('day 33 started');
 
+//remove vowels from string
+function removevowelinstr(str){
+    let x = str.replace(/[aeiou]/gi,'');
+    console.log(x)
+}   
+removevowelinstr('i am suraj bhai');
+
 
 console.log('day 33 started')
