877f729d07547db897ad62f62e5a27a96e8e5c8b
Gupta <suraj.gupta@capgemini.com>
2025-11-25 11:16:59 +0530

    day 38 added

--- day38.js
+++ day38.js
@@ -1,4 +1,104 @@
 console.log("Day 38 started");
 
+// star patterns
+
+//1
+// *
+// **
+// ***
+// ****
+// *****
+
+// for(let i=1;i<=5;i++){
+//     let str = ''
+//     for(let j=5;j<=i+4;j++){
+//         str = str + '*'
+//     }
+//     console.log(str)
+// }
+
+
+//2
+// *****
+// *****
+// *****
+// *****
+// *****
+
+
+// for(let i=1;i<=5;i++){
+//     let str = ""
+//     for(let j=1;j<=5;j++){
+//         str = str + "*"
+//     }
+//     console.log(str)
+// }
+
+//3
+// *****
+// ****
+// ***
+// **
+// *
+
+
+// for(let i=1;i<=5;i++){
+//     let str = "";
+//     for(let j=5;j<=10-i;j++){
+//         str = str + "*"
+//     }
+//     console.log(str)
+// }
+
+//4
+// Right-Angled Triangle (Right-Aligned)
+//     *
+//   **
+//   ***
+//  ****
+// *****
+
+
+// for(let i=1;i<=5;i++){
+//     let str = "";
+//     for(let j=5;j<=9-i;j++){
+//         str = str + ' '
+//     }
+    
+   
+//      for(let j=5;j<=4+i;j++){
+//         str = str + '*'
+//     }
+//      console.log(str)
+// }
+
+//5
+// 5) Centered Pyramid (Solid)
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+
+
+
+// for(let i=1;i<=5;i++){
+//     let str = "";
+//     for(let j=5;j<=9-i;j++){
+//         str = str + ' '
+//     }
+    
+   
+//      for(let j=5;j<=4+i;j++){
+//         str = str + '*'
+//     }
+//     for(let j=5;j<=3+i;j++){
+//         str = str + '*'
+//     }
+//      console.log(str)
+// }
+
+
+
 
 console.log("Day 38 ended");
