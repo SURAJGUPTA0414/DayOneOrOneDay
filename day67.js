7c79361b78e055e2b613e39c256bdd926ca0d7ad
suraj gupta <sandygupta0414@gmail.com>
2025-12-25 20:09:28 +0530

    day67 done

--- day67.js
+++ day67.js
@@ -1,3 +1,60 @@
 console.log("day67 started");
 
+//1
+// sai krishna
+
+// function removeDuplicate(arr){
+    
+//     let x = arr.filter((a,b)=>arr.indexOf(a) === b);
+//     console.log(x)
+    
+// }
+// removeDuplicate([1,3,2,1,5])
+
+//2
+// function findEvenNumber(arr){
+//     let even = arr.filter((x)=>x%2===0);
+//     console.log(even)
+// }
+// findEvenNumber([1,2,1,4])
+
+//3
+// function isPalindrome(str){
+//     if(str === str.split('').reverse().join('')){
+//         console.log('palindrome')
+//     }else{
+//         console.log('not palindrome')
+//     }
+// }
+
+// isPalindrome('madam')
+
+//4
+// function factorial(n){
+//     let fact = 1;
+//     for(let i=1;i<=n;i++){
+//         fact = fact * i
+//     }
+//     console.log(fact)
+// }
+
+// factorial(5)
+
+
+//5
+function findLongestString(word){
+    let words = word.split(' ');
+    
+    let longest = words[0];
+    
+    for(let dd of words){
+        if(dd>longest){
+            longest = dd
+        }
+    }
+    console.log(longest)
+}
+
+findLongestString('suraj is great')
+
 console.log("day67 ended");
