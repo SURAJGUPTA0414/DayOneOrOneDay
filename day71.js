4554445a93bb0753c701f2bacdf760cba40c6b0c
suraj gupta <sandygupta0414@gmail.com>
2025-12-29 19:51:42 +0530

    lets add

--- day71.js
+++ day71.js
@@ -1,4 +1,22 @@
 console.log('day71  started');
+//1 
+// swap variable all three methods
+
+let a = 10;
+let b = 20;
+
+// let temp = a;   
+// a = b
+// b = temp;
+
+
+// [a,b] = [b,a]
+
+a = a+b;
+b = a-b;
+a = a-b;
+console.log(a,b)
+
 
 
 console.log('day71 ended')
