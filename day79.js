97f6dfa44bccf7f52f6b9d396e20da365f2d6f2c
suraj gupta <sandygupta0414@gmail.com>
2026-01-05 18:15:17 +0530

    added for polyfills

--- day79.js
+++ day79.js
@@ -1,4 +1,49 @@
 console.log('day79 started');
 
+Array.prototype.myFilter = function (callback){
+    let result = [];
+    
+    for(let i=0;i<this.length;i++){
+        if(callback(this[i])){
+            result.push(this[i])
+        }
+    }
+    return result;
+}
+
+
+Array.prototype.maMap = function (callback){
+    let result = [];
+    
+    for(let i=0;i<this.length;i++){
+       
+            result.push(callback(this[i]))
+        
+    }
+    return result;
+}
+
+Array.prototype.myReduce = function (callback,initialvalue){
+    let acc = initialvalue;
+    let start = 0;
+    
+    if(acc === undefined){
+        acc= this[0];
+        start = 1
+    }
+    
+    for(let i=0;i<this.length;i++){
+       
+            acc = callback(acc,this[i])
+        
+    }
+    return acc;
+}
+
+
+let arr = [2,4,2,8,1];
+
+console.log(arr.myReduce((a,b)=>a+b))
+
 
 console.log('day79 ended');
